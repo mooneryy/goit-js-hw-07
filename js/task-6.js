b9fed3b40e3193250419f1ded4e57bdf4691df14
mooneryy <yelisyeyevaa@gmail.com>
2024-01-26 23:05:19 +0200

    add

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  const boxesContainer = document.getElementById('boxes');
+  boxesContainer.innerHTML = '';
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.cssText = `width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()};`
+    boxesContainer.appendChild(box);
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  const boxesContainer = document.getElementById('boxes');
+  boxesContainer.innerHTML = '';
+}
+
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const input = document.querySelector('#controls input[type = "number"]');
+
+createButton.addEventListener('click', () => {
+  const amount = parseInt(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+  }
+  input.value = '';
+});
+
+destroyButton.addEventListener('click', destroyBoxes);
